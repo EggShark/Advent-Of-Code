89b5c61376c20175ea994b1e7041581bc8988ea9
TheEggShark <ther3al3ggshark@gmail.com>
2022-11-30 23:11:44 -0600

    day1 9:23

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -6,8 +6,37 @@ use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+
+    let input = read_to_string("./texts/day01.txt").unwrap();
+    let tool: Vec<&str> = input.split("\n").collect();
+
+    println!("{:?}", tool);
+
+    let mut largest_sum = 0;
+    let mut current_sum = 0;
+
+    let mut sum2 = 0;
+    let mut sum3 = 0;
+
+    for number in tool {
+        if number == "" {
+            current_sum = 0;
+        } else {
+            let u: u64 = number.parse().unwrap();
+            current_sum += u;
+            if current_sum > largest_sum {
+                largest_sum = current_sum;
+            } else if current_sum > sum2 {
+                sum2 = current_sum;
+            } else if current_sum > sum3 {
+                sum3 = current_sum;
+            }
+        }
+
+    }
+
+    let sol1: u64 = largest_sum;
+    let sol2: u64 = largest_sum + sum2 + sum3;
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
