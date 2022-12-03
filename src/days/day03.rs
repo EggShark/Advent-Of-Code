4e119415fe8bb27231cb47dd01d860f9351d2fab
TheEggShark <ther3al3ggshark@gmail.com>
2022-12-03 09:48:03 -0600

    day3 ~47:00

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -5,8 +5,62 @@ use std::fs::read_to_string;
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("./texts/day03.txt").unwrap();
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let x: u32 = input.split("\n").collect::<Vec<&str>>()
+        .iter()
+        .map(|g| {
+            let g = g.to_string();
+            let (c1, c2) = g.split_at(g.len()/2);
+            let mut priority = 0;
+            for c in c1.chars() {
+                if c2.contains(c) {
+                    priority += get_prority(c);
+                    break;
+                }
+            }
+            priority
+        }).sum();
+
+        let mut group_vec: Vec<&str> = Vec::new();
+        let mut counter = 0;
+        for line in input.lines() {
+            if group_vec.len() != 3 {
+                group_vec.push(line);
+            } else {
+                for c in group_vec[0].chars() {
+                    if group_vec[1].contains(c) && group_vec[2].contains(c) {
+                        counter += get_prority(c);
+                        break;
+                    }
+                }
+                group_vec = Vec::new();
+                group_vec.push(line);
+            }
+        }
+        for c in group_vec[0].chars() {
+            if group_vec[1].contains(c) && group_vec[2].contains(c) {
+                counter += get_prority(c);
+                break;
+            }
+        }
+        
+
+    let sol1: u32 = x;
+    let sol2: u32 = counter;
+
+    (Solution::U32(sol1), Solution::U32(sol2))
+}
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+fn get_prority(c: char) -> u32 {
+    let mut pos = 0;
+    for a in ALPHABET.chars() {
+        pos += 1;
+        if a == c {
+            break
+        }
+    }
+    pos
 }
