bec70889d60806befe3a6f13af0bdbef305f3fb2
TheEggShark <ther3al3ggshark@gmail.com>
2022-12-04 00:00:09 -0600

    day 4 52:18

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -5,11 +5,46 @@ use std::time::Instant;
 
 pub fn solve() -> Solution {
     // Your solution here...
-    let text = read_to_string("").unwrap();
+    let text = read_to_string("./texts/day04.txt").unwrap();
     let time = Instant::now();
 
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+
+    let mut vec1 = Vec::new();
+
+    for line in text.lines() {
+        let z = line.split(",");
+        for s in z {
+            vec1.push(s);
+        }
+    }
+
+    let mut vec2 = Vec::new();
+    for s in vec1.iter() {
+        let z = s.split("-").collect::<Vec<&str>>();
+        vec2.push([z[0].parse::<u32>().unwrap(), z[1].parse::<u32>().unwrap()]);
+    }
+    drop(vec1);
+
+    let mut pos_counter = 0;
+    let mut counter = 0;
+    let mut counter2 = 0;
+
+    while pos_counter < vec2.len() - 1 {
+        let a = vec2[pos_counter][0];
+        let b = vec2[pos_counter][1];
+        let c = vec2[pos_counter+1][0];
+        let d = vec2[pos_counter+1][1];
+        if (a <= c && b >= d) || (a >= c && b <= d) {
+            counter += 1;
+        }
+        if b >= c && d >= a {
+            counter2 += 1;
+        }
+        pos_counter += 2;
+    }
+
+    let sol1: u64 = counter;
+    let sol2: u64 = counter2;
 
     let solution = (SolutionType::U64(sol1), SolutionType::U64(sol2));
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
