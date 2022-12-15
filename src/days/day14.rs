ce96374ae95998ab22f824b5040025be0a34d76b
TheEggShark <ther3al3ggshark@gmail.com>
2022-12-14 20:44:24 -0600

    day 14 i did this one on my own

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,33 +1,44 @@
 use crate::{Solution, SolutionType};
 use std::fs::read_to_string;
-use std::ops::Range;
+use std::ops::RangeInclusive;
 use std::time::Instant;
 ///////////////////////////////////////////////////////////////////////////////
 
 type Map = [[Space; 1000]; 200];
+const SANDDROPOFF: Point = Point{x: 500, y: 0};
 
 pub fn solve() -> Solution {
     // Your solution here...
     let text = read_to_string("./texts/day14.txt").unwrap();
     let time = Instant::now();
 
-    let mut grid: Map = [[Space::Air; 1000]; 200];
+    let mut grid = build_cave(&text);
+    let mut grid2 = grid;
+    add_floor(&mut grid2);
 
-    let mut lines = text.lines();
-    let _ = lines.next().unwrap();
-    let line = lines.next().unwrap();
+    let mut counter = 0;
 
-    let x = line.split(" -> ").map(|s|s.to_string())
-        .collect::<Vec<String>>()
-        .iter()
-        .map(|point| Point::from_str(point))
-        .collect::<Vec<Point>>();
+    loop {
+        if drop_sand(&mut grid) {
+            counter += 1;
+        } else {
+            break;
+        }
+    }
 
-    println!("{:?}", x);
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let mut c2 = 0;
+    loop {
+        if drop_sand(&mut grid2) {
+            c2 += 1;
+        } else {
+            break;
+        }
+    }
 
-    let solution = (SolutionType::U64(sol1), SolutionType::U64(sol2));
+    let sol1: i32 = counter;
+    let sol2: i32 = c2 + 1; // the code above doesnt count the last bit of sand
+
+    let solution = (SolutionType::I32(sol1), SolutionType::I32(sol2));
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solution {
         solution,
@@ -35,7 +46,39 @@ pub fn solve() -> Solution {
     }
 }
 
-fn build_cave(input: String) -> Map {
+fn drop_sand(grid: &mut Map) -> bool {
+    let mut pos = SANDDROPOFF;
+    loop {
+        if pos.y == 199 {
+            return false; // into the void
+        }
+        let bellow = grid[(pos.y + 1) as usize][(pos.x) as usize];
+        let left = grid[(pos.y + 1) as usize][(pos.x - 1) as usize];
+        let right = grid[(pos.y + 1) as usize][(pos.x + 1) as usize];
+        if bellow == Space::Air {
+            pos.y += 1;
+        } else {
+            //try left then right
+            if left == Space::Air {
+                pos.y += 1;
+                pos.x -= 1;
+            } else if right == Space::Air {
+                pos.y += 1;
+                pos.x += 1;
+            } else {
+                // hes resting
+                grid[pos.y as usize][pos.x as usize] = Space::Sand;
+                if pos == SANDDROPOFF {
+                    return false;
+                }
+                break;
+            }
+        }
+    }
+    true
+}
+
+fn build_cave(input: &String) -> Map {
     let mut grid: Map = [[Space::Air; 1000]; 200];
 
     let lines = input.lines();
@@ -45,13 +88,59 @@ fn build_cave(input: String) -> Map {
         .iter()
         .map(|point| Point::from_str(point))
         .collect::<Vec<Point>>();
-        let iter = shape.iter();
+
+        let mut iter = shape.iter();
+        let mut prev = iter.next().unwrap();
+        grid[prev.y as usize][prev.x as usize] = Space::Rock;
+        while let Some(new_point) = iter.next() {
+            if prev.x > new_point.x {
+                // 30..10
+                let range = new_point.get_x_range(*prev);
+                for new_x in range {
+                    grid[prev.y as usize][new_x as usize] = Space::Rock;
+                }
+            } else if prev.x < new_point.x {
+                //10..30
+                let range = prev.get_x_range(*new_point);
+                for new_x in range {
+                    grid[prev.y as usize][new_x as usize] = Space::Rock;
+                }
+            } else if prev.y > new_point.y {
+                let range = new_point.get_y_range(*prev);
+                for new_y in range {
+                    grid[new_y as usize][prev.x as usize] = Space::Rock;
+                }
+            } else {
+                // prev_y < y
+                let range = prev.get_y_range(*new_point);
+                for new_y in range {
+                    grid[new_y as usize][prev.x as usize] = Space::Rock;
+                }
+            }
+            prev = new_point;
+        }
+        grid[prev.y as usize][prev.x as usize] = Space::Rock;
     }
 
     grid
 }
 
-#[derive(Copy, Clone ,Debug, PartialEq)]
+fn add_floor(grid: &mut Map) {
+    let mut largest_y = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == Space::Rock && y > largest_y {
+                largest_y = y;
+            }
+        }
+    }
+    let floor_height = largest_y + 2;
+    for x in 0..1000 {
+        grid[floor_height][x] = Space::Rock;
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Space {
     Air,
     Rock,
@@ -73,7 +162,11 @@ impl Point {
         }
     }
 
-    pub fn get_ranges(&self, other: Point) -> (Range<i32>, Range<i32>) {
-        (self.x..other.x, self.y..other.y)
+    pub fn get_x_range(&self, other: Point) -> RangeInclusive<i32> {
+        self.x..=other.x
+    }
+
+    pub fn get_y_range(&self, other: Point) -> RangeInclusive<i32> {
+        self.y..=other.y
     }
 }
