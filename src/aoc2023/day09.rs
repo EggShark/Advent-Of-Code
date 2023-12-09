fabd91f2193a4fcbce5c2b4730491fe69c2897c7
TheEggShark <ther3al3ggshark@gmail.com>
2023-12-09 12:46:25 -0600

    clean up and slight opitmization of day 09

--- src/aoc2023/day09.rs
+++ src/aoc2023/day09.rs
@@ -8,16 +8,20 @@ pub fn solve() -> Solution {
     let text = read_to_string("src/aoc2023/texts/day9").unwrap();
     let time = Instant::now();
 
-    let histories: i32 = text.lines().map(|line| {
+    let histories: i32 = text
+    .lines()
+    .map(|line| {
         line.split_whitespace().map(|thing| thing.parse::<i32>().unwrap()).collect::<Vec<i32>>()
     })
-    .map(|mut history| get_next_value(&mut history, 0))
+    .map(|history| get_next_value(&history, 0))
     .sum();
 
-    let rev_histories = text.lines().map(|line| {
+    let rev_histories = text
+    .lines()
+    .map(|line| {
         line.split_whitespace().map(|thing| thing.parse::<i32>().unwrap()).collect::<Vec<i32>>()
     })
-    .map(|mut history| get_first_value(&mut history, 0))
+    .map(|history| get_first_value(&history, 0))
     .sum();
 
     let sol1: i32 = histories;
@@ -31,7 +35,7 @@ pub fn solve() -> Solution {
     }
 }
 
-fn get_next_value(array: &mut Vec<i32>, depth: i32) -> i32 {
+fn get_next_value(array: &[i32], depth: i32) -> i32 {
     let mut vec = vec![0_i32; array.len()-1];
 
     for idx in 0..array.len()-1 {
@@ -42,15 +46,13 @@ fn get_next_value(array: &mut Vec<i32>, depth: i32) -> i32 {
     let z = if vec.windows(2).all(|w| w[0] == w[1]) {
         vec[0]
     } else {
-        get_next_value(&mut vec, depth + 1)
+        get_next_value(&vec, depth + 1)
     };
 
-    array.push(z + *array.last().unwrap());
-
-    *array.last().unwrap()
+    array.last().unwrap() + z
 }
 
-fn get_first_value(array: &mut Vec<i32>, depth: i32) -> i32 {
+fn get_first_value(array: &[i32], depth: i32) -> i32 {
     let mut vec = vec![0_i32; array.len()-1];
 
     for idx in 0..array.len()-1 {
@@ -61,9 +63,8 @@ fn get_first_value(array: &mut Vec<i32>, depth: i32) -> i32 {
     let z = if vec.windows(2).all(|w| w[0] == w[1]) {
         vec[0]
     } else {
-        get_first_value(&mut vec, depth + 1)
+        get_first_value(&vec, depth + 1)
     };
 
-    array.insert(0, *array.first().unwrap() - z);
-    *array.first().unwrap()
+    array[0] - z
 }
