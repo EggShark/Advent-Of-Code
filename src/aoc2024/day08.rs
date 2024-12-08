eb4e6550c2d087e83eb9e1a1a5abb3779dad8e7b
EggShark <eggshark@eggshark.dev>
2024-12-07 22:53:44 -0700

    I'm suprised I did this one

--- src/aoc2024/day08.rs
+++ src/aoc2024/day08.rs
@@ -1,15 +1,46 @@
 use crate::Solve;
-use std::time::Instant;
+use std::{collections::{HashMap, HashSet}, time::Instant};
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn part1(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
 
+    // its square
+    let size = data_in.lines().count();
+
+    let mut positions: HashMap<u8, Vec<(i32, i32)>> = HashMap::new();
+    for (y, line) in data_in.lines().enumerate() {
+        for (x, char) in line.bytes().enumerate() {
+            if char != b'.' {
+                let to_edit = positions.entry(char).or_default();
+                to_edit.push((x as i32, y as i32));
+            }
+        }
+    }
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+    for (_, v) in positions {
+        for i in 0..v.len() {
+            let (x1, y1) = v[i];
+            for j in i+1..v.len() {
+                let (x2, y2) = v[j];
+                let dx = x1 - x2;
+                let dy = y1 - y2;
+
+                if x1 + dx >= 0 && x1 + dx < size as i32 && y1 + dy >= 0 && y1 + dy < size as i32 {
+                    antinodes.insert((x1 + dx, y1 + dy));
+                }
+
+                if x2 - dx >= 0 && x2 - dx < size as i32 && y2 - dy >= 0 && y2 - dy < size as i32 {
+                    antinodes.insert((x2 - dx, y2 - dy));
+                }
+            }
+        }
+    }
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
-        solution: Box::new(solve),
+        solution: Box::new(antinodes.len()),
         time_ms,
     }
 }
@@ -19,9 +50,54 @@ pub fn part2(data_in: &str) -> Solve {
     let solve = 0;
 
 
+    let size = data_in.lines().count();
+
+    let mut positions: HashMap<u8, Vec<(i32, i32)>> = HashMap::new();
+    for (y, line) in data_in.lines().enumerate() {
+        for (x, char) in line.bytes().enumerate() {
+            if char != b'.' {
+                let to_edit = positions.entry(char).or_default();
+                to_edit.push((x as i32, y as i32));
+            }
+        }
+    }
+
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+    for (_, v) in positions {
+        for i in 0..v.len() {
+            let (x1, y1) = v[i];
+            for j in i+1..v.len() {
+                let (x2, y2) = v[j];
+                let dx = x1 - x2;
+                let dy = y1 - y2;
+
+                let mut mult = 0;
+                loop {
+                    if x1 + (dx * mult) >= 0 && x1 + (dx * mult) < size as i32 && y1 + (dy * mult) >= 0 && y1 + (dy * mult) < size as i32 {
+                        antinodes.insert((x1 + dx * mult, y1 + dy * mult));
+                    } else {
+                        break;
+                    }
+                    mult += 1;
+                    
+                }
+                mult = 0;
+
+                loop {
+                    if x2 - (dx * mult) >= 0 && x2 - (dx * mult) < size as i32 && y2 - (dy * mult) >= 0 && y2 - (dy * mult) < size as i32 {
+                        antinodes.insert((x2 - dx * mult, y2 - dy * mult));
+                    } else {
+                        break;
+                    }
+                    mult += 1;
+                }
+            }
+        }
+    }
+
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
-        solution: Box::new(solve),
+        solution: Box::new(antinodes.len()),
         time_ms,
     }
 }
