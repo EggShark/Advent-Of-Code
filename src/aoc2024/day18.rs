6283f30d954c8c63fded8e2fa2cd92048449a50e
EggShark <ther3al3ggshark@gmail.com>
2024-12-18 13:38:40 -0600

    binary search optimization

--- src/aoc2024/day18.rs
+++ src/aoc2024/day18.rs
@@ -50,52 +50,62 @@ pub fn part1(data_in: &str) -> Solve {
 pub fn part2(data_in: &str) -> Solve {
     let time = Instant::now();
 
-    let mut grid = [[Tile::Empty; GRID_SIZE]; GRID_SIZE];
     let data_in = data_in.
         lines()
         .filter_map(|d| d
             .split_once(',')
             .and_then(|(l, r)| Some((l.parse::<usize>().unwrap(), r.parse::<usize>().unwrap())))
-        );
+        )
+        .collect::<Vec<(usize, usize)>>();
 
-    data_in.clone().take(1024).for_each(|(x, y)| grid[y][x] = Tile::Courupted);
-    let mut last = (0,0);
-
-    'a: for (cx, cy) in data_in.skip(1024) {
-        grid[cy][cx] = Tile::Courupted;
-
-        let mut stack = vec![];
-        let mut seen = HashSet::new();
-        seen.insert((0, 0));
-        stack.push((0,0));
-        while let Some((x, y)) = stack.pop() {
-            if (x, y) == (70, 70) {
-                continue 'a;
-            }
-
-            for (dx, dy) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
-                if x + dx >= 0 && y + dy >= 0
-                    && x + dx < GRID_SIZE as i32 && y + dy < GRID_SIZE as i32
-                    && grid[(y + dy) as usize][(x + dx) as usize] == Tile::Empty
-                    && seen.insert((x + dx, y + dy))
-                {
-                    stack.push((x + dx, y + dy));
-                }
-            }
-        }
-
-
-
-        last = (cx, cy);
-        break;
+    let mut original_grid = [[Tile::Empty; GRID_SIZE]; GRID_SIZE];
+    for &(x, y) in data_in.iter().take(1024) {
+        original_grid[y][x] = Tile::Courupted;
     }
 
+    let mut min = 1024;
+    let mut max = data_in.len() - 1;
+    while min <= max {
+        let idx = ((max-min) / 2) + min;
+        let mut c = original_grid;
+        data_in.iter().skip(1024).take(idx-1024).for_each(|&(x,y)| c[y][x] = Tile::Courupted);
+        
+        if way_out(&c) {
+            min = idx + 1;
+        } else {
+            max = idx - 1;
+        }
+    }
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
-        solution: Box::new(format!("{:?}", last)),
+        solution: Box::new(format!("{},{}",data_in[max].0, data_in[max].1)),
         time_ms,
     }
+}
+
+fn way_out(grid: &[[Tile; GRID_SIZE]; GRID_SIZE]) -> bool {
+    let mut seen = HashSet::new();
+    let mut stack = vec![];
+    stack.push((0,0));
+    seen.insert((0,0));
+    while let Some((x, y)) = stack.pop() {
+        if (x, y) == (70, 70) {
+            return true;
+        }
+
+        for (dx, dy) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+            if x + dx >= 0 && y + dy >= 0
+                && x + dx < GRID_SIZE as i32 && y + dy < GRID_SIZE as i32
+                && grid[(y + dy) as usize][(x + dx) as usize] == Tile::Empty
+                && seen.insert((x + dx, y + dy))
+            {
+                stack.push((x + dx, y + dy));
+            }
+        }
+    }
+
+    false
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
