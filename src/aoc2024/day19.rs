24e72c24832d91c22f52977b0856173c573f49eb
EggShark <ther3al3ggshark@gmail.com>
2024-12-19 11:24:34 -0600

    really need to not run in release

--- src/aoc2024/day19.rs
+++ src/aoc2024/day19.rs
@@ -1,10 +1,23 @@
 use crate::Solve;
-use std::time::Instant;
+use std::{cmp::min, collections::{HashMap, HashSet}, time::Instant};
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn part1(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
+    let mut solve = 0;
+
+    let mut lines = data_in.lines();
+    let patterns = lines.next().unwrap().split(", ");
+    let max_len = patterns.clone().map(|d| d.len()).max().unwrap();
+    let patterns = patterns.collect::<HashSet<_>>();
+    let mut cache = HashMap::new();
+
+    
+    for design in lines.skip(1) {
+        if good_towel(design, &patterns, max_len, &mut cache) {
+            solve += 1;
+        }
+    }
 
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
@@ -16,7 +29,18 @@ pub fn part1(data_in: &str) -> Solve {
 
 pub fn part2(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
+    let mut solve: u64 = 0;
+
+    let mut lines = data_in.lines();
+    let patterns = lines.next().unwrap().split(", ");
+    let max_len = patterns.clone().map(|d| d.len()).max().unwrap();
+    let patterns = patterns.collect::<HashSet<_>>();
+    let mut cache = HashMap::new();
+    
+    
+    for design in lines.skip(1) {
+        solve += count_towel(design, &patterns, max_len, &mut cache);
+    }
 
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
@@ -24,4 +48,46 @@ pub fn part2(data_in: &str) -> Solve {
         solution: Box::new(solve),
         time_ms,
     }
+}
+
+fn good_towel<'a>(towel: &'a str, patterns: &HashSet<&str>, max_len: usize, cache: &mut HashMap<&'a str, bool>) -> bool {
+    if towel.len() == 0 {
+        return true;
+    }
+
+    if let Some(n) = cache.get(towel) {
+        return *n;
+    }
+
+    let loop_amount = min(towel.len(), max_len) + 1;
+    for i in 0..loop_amount {
+        if patterns.contains(&towel[..i]) && good_towel(&towel[i..], patterns, max_len, cache) {
+            cache.insert(towel, true);
+            return true;
+        }
+    }
+
+    cache.insert(towel, false);
+    false
+}
+
+fn count_towel<'a>(towel: &'a str, patterns: &HashSet<&str>, max_len: usize, cache: &mut HashMap<&'a str, u64>) -> u64 {
+    if towel.len() == 0 {
+        return 1;
+    }
+
+    if let Some(n) = cache.get(towel) {
+        return *n;
+    }
+
+    let mut counter = 0;
+    let loop_amount = min(towel.len(), max_len) + 1;
+    for i in 0..loop_amount {
+        if patterns.contains(&towel[..i]) {
+            counter += count_towel(&towel[i..], patterns, max_len, cache);
+        }
+    }
+
+    cache.insert(towel, counter);
+    counter
 }
