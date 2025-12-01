34bc275bc49f06716c2bcb2423a30eeb52a18f47
EggShark <eggshark@eggshark.dev>
2025-11-30 23:22:29 -0700

    im gonna crash out

--- src/aoc2025/day01.rs
+++ src/aoc2025/day01.rs
@@ -4,7 +4,25 @@ use std::time::Instant;
 
 pub fn part1(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
+    let mut solve = 0;
+
+    let mut pos = 50;
+    for line in data_in.lines() {
+        let i = line[1..].parse::<i32>().unwrap();
+        match &line[0..1] {
+            "L" => {
+               pos = (pos - i).rem_euclid(100);
+            },
+            "R" => {
+                pos = (pos + i).rem_euclid(100);
+            }
+            _ => unreachable!() 
+        }
+
+        if pos == 0 {
+            solve += 1;
+        }
+    }
 
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
@@ -16,8 +34,33 @@ pub fn part1(data_in: &str) -> Solve {
 
 pub fn part2(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
+    let mut solve = 0;
 
+    let mut pos = 50;
+    for line in data_in.lines() {
+        let i = line[1..].parse::<i32>().unwrap();
+        match &line[0..1] {
+            "L" => {
+                solve += i/100;
+                println!("{}", pos - i);
+                if pos != 0 && i % 100 >= pos {
+                    println!("count down");
+                    solve += 1
+                    
+                }
+                pos = (pos - i).rem_euclid(100);
+            },
+            "R" => {
+                println!("{}", pos + i);
+                if pos + i >= 100 {
+                    println!("count up");
+                    solve += (pos + i)/100;
+                }
+                pos = (pos + i).rem_euclid(100);
+            }
+            _ => unreachable!() 
+        }
+    }
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
