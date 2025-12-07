26da6eb6534527a56656151e2ce609ca014cb302
EggShark <eggshark@eggshark.dev>
2025-12-07 14:27:59 -0700

    day 7 p2

--- src/aoc2025/day07.rs
+++ src/aoc2025/day07.rs
@@ -1,6 +1,6 @@
 use crate::Solve;
 
-use std::{collections::HashSet, time::Instant};
+use std::{collections::{HashMap, HashSet}, time::Instant};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Tile {
@@ -54,9 +54,8 @@ pub fn part1(data_in: &str) -> Solve {
 
 pub fn part2(data_in: &str) -> Solve {
     let time = Instant::now();
-    let mut solve = 0;
 
-    let mut grid = data_in.lines().map(|l| l.bytes().map(|b| match b {
+    let grid = data_in.lines().map(|l| l.bytes().map(|b| match b {
         b'S' => Tile::Start,
         b'.' => Tile::Empty,
         b'|' => Tile::Beam,
@@ -66,37 +65,32 @@ pub fn part2(data_in: &str) -> Solve {
     .collect::<Vec<_>>();
 
     let start_x = grid[0].len() / 2;
-    grid[0][start_x] = Tile::Beam;
 
-    let mut pos = Vec::new();
-    let mut seen_shit = HashSet::new();
-    pos.push((0, start_x));
+    let mut map = HashMap::new();
 
-    while let Some((y, x)) = pos.pop() {
-        if y >= grid.len() - 2 {
-            solve += 1;
-        }
-        for ys in y+1..grid.len() {
-            if grid[ys][x] == Tile::Spliter {
-                // hash tile and just auto solve += 2;
-                // or also count how many times we get to the bottom splitters * 2 idk
-                // keep track of # of splits and * 2 at end for score per beam ig?
-                if seen_shit.insert((ys, x+1)) {
-                    pos.push((ys, x+1));
-                    solve += 1;
-                }
-                if seen_shit.insert((ys, x-1)) {
-                    pos.push((ys, x-1));
-                    solve += 1;
-                }
-                break;
-            }
-        }
-    }
+    let solve = dfs_memoized((0, start_x), &grid, &mut map);
+
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
         solution: Box::new(solve),
         time_ms,
     }
+}
+
+fn dfs_memoized((y, x): (usize, usize), grid: &[Vec<Tile>], map: &mut HashMap<(usize, usize), u64>) -> u64 {
+    if let Some(&v) = map.get(&(y, x)) {
+        return v;
+    }
+
+    let time_lines = if let Some ((ys, _)) = grid.iter().enumerate().skip(y+1).find(|(_, r)| r[x] == Tile::Spliter) {
+        dfs_memoized((ys, x+1), grid, map) + dfs_memoized((ys, x-1), grid, map)
+    } else {
+        1
+    };
+
+
+    map.insert((y,x), time_lines);
+
+    time_lines
 }
