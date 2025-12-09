e732fffcf999f3dc636d3a4a2a3e02dc10bac5db
EggShark <eggshark@eggshark.dev>
2025-12-08 23:17:47 -0700

    easy p1 worry about p2 tomorrow

--- src/aoc2025/day09.rs
+++ src/aoc2025/day09.rs
@@ -4,8 +4,21 @@ use std::time::Instant;
 
 pub fn part1(data_in: &str) -> Solve {
     let time = Instant::now();
-    let solve = 0;
 
+    let points = data_in.lines().map(|l| {
+        let (l, r) = l.split_once(',').unwrap();
+        (l.parse().unwrap(),r.parse().unwrap())
+    }).collect::<Vec<(i64, i64)>>();
+
+    let mut max_area = 0;
+    for &(x1, y1) in points.iter() {
+        for &(x2, y2) in points.iter() {
+            let area = ((x1-x2+1)*(y1-y2+1)).abs();
+            max_area = std::cmp::max(area, max_area);
+        }
+    }
+
+    let solve = max_area;
 
     let time_ms = time.elapsed().as_nanos() as f64 / 1000000.0;
     Solve {
